11ee05a1a344537e10bfafd2a658df7bc96c0a3d
ijacklima <jacklimavas@gmail.com>
2022-05-18 11:21:43 -0300

    Added variables

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
-console.log('ola')
+const pwE = document.getElementById("pw");
+const copyE = document.getElementById("copy");
+const lenE = document.getElementById("len");
+const upperE = document.getElementById("upper");
+const lowerE = document.getElementById("lower");
+const numberE = document.getElementById("number");
+const symbolE = document.getElementById("symbol");
+const generateE = document.getElementById("generate");
+
+const upperLetters = "ABCDEFGHIJKLMNOPQRSTUVWYZ";
+const lowerLetters = "abcdefghijklmnopqrstuvwyz";
+const numbers = "0123456789";
+const symbols = "!@#$%^&*()_+="
